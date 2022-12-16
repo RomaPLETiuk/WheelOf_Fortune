14805d4ca5a9ebb0619c4eeff60b0fcdcc98b5a3
RomaPLETiuk <108412429+RomaPLETiuk@users.noreply.github.com>
2022-12-16 17:38:22 +0300

    Delete rev.js

--- js/rev.js
+++
@@ -1,8 +0,0 @@
-function run(){
-    var userText=document.querySelector('#userText').value;
-    var ul=document.querySelector('ul');
-    var li=document.createElement('li');
-    li.innerText=userText.split("").reverse().join("");
-        ul.appendChild(li);
-//return alert(userText.split("").reverse().join(""));
-}
